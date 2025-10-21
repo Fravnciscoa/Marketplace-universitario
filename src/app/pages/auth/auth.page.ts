db4f4c086661065754f8cfedf1742b1e702385ee
Fravncisco <fravn.saavedra@gmail.com>
2025-10-20 21:17:36 -0300

    feat(backend): estructura base + conexión a BD (EP2.1 - EP2.2) (#17)
    * feat: páginas base (home, perfil, detalle-producto, auth) + rutas
    
    * feat(home): header mínimo y placeholder de contenido
    
    * feat(home): layout base con searchbar + placeholders de filtros y grid
    
    * feat(home): mocks en memoria + grid responsivo con cards
    
    * chore(home): habilita FormsModule y normaliza inputs numéricos
    
    * fix(home): ion-segment usa ngModel para estadoSel
    
    * fix(home): ion-segment usa ngModel para estadoSel
    
    * chore: format with prettier
    
    * feat(home): WIP rediseño base + asset demo
    
    * feat: agrega estructura del backend, conexión DB y endpoint /health funcional
    
    * feat: estructura base backend y conexión DB funcionando

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
-  template: `
-    <ion-header>
-      <ion-toolbar color="primary">
-        <ion-title>Auth</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content class="ion-padding">
-      <p>Auth page shell ✅</p>
-      <ion-button routerLink="/" fill="outline" size="small"
-        >Ir a Home</ion-button
-      >
-    </ion-content>
-  `,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
-export class AuthPage {}
+export class AuthPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
