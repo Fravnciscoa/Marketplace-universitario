e662b82348493dc79818a8afc71933a43b00f31e
Cboyeah <sebaflash735@gmail.com>
2025-11-15 19:51:03 -0300

    Se crea el servicio carrito para que puedan agregarse productos a elección en el carrito de compras

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -8,7 +8,7 @@ export const pool = new Pool({
   port: Number(process.env.DB_PORT) || 5432,
   database: process.env.DB_DATABASE || 'IngWeb',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'Akira1712',
+  password: process.env.DB_PASSWORD,
   ssl: false
 });
 
